df649f23c46df4d29f726b122dacb046d05731e8
LuizHSilvaF <luiz.henriqueoff10@gmail.com>
2024-10-31 21:07:44 -0300

    Adicionando mais um useState de exemplo

--- 3_AVANCANDO_REACT/SessaoTres/src/components/ManageData.jsx
+++ 3_AVANCANDO_REACT/SessaoTres/src/components/ManageData.jsx
@@ -5,6 +5,8 @@ const ManageData = () => {
 
     const [number, setNumber] = useState(15);
 
+    const [usuario, setUsuario] = useState({nome: 'Luiz', idade: 19});
+
     return (
     <div>
         <div>
@@ -14,6 +16,12 @@ const ManageData = () => {
         <div>
             <p>Valor: {number}</p>
             <button onClick={() => setNumber(25)}>Mudar Valor</button>
+        </div>
+        <div>
+            <p>Nome: {usuario.nome}</p>
+            <button onClick={() => {
+                setUsuario({...usuario, nome: 'Luiza'})
+            }}>Muda Nome</button>
         </div>
     </div>
     )
