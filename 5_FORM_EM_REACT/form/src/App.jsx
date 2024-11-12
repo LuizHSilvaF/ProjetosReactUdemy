4b0bbb68119b9562df811ab2092bac17f20dff09
LuizHSilvaF <luiz.henriqueoff10@gmail.com>
2024-11-12 15:49:34 -0300

    Adicionando Modulo de Select Option

--- 5_FORM_EM_REACT/form/src/App.jsx
+++ 5_FORM_EM_REACT/form/src/App.jsx
@@ -5,7 +5,7 @@ function App() {
    return (
     <>
       <h1>Forms</h1>
-      <MyForm user={{name: "Luiz", email: "luiz@gmail.com"}}/>
+      <MyForm user={{name: "Luiz", email: "luiz@gmail.com", bio: "Bio", role: "selecione"}}/>
     </>
   )
 }
--- 5_FORM_EM_REACT/form/src/components/MyForm.jsx
+++ 5_FORM_EM_REACT/form/src/components/MyForm.jsx
@@ -8,7 +8,8 @@ const MyForm = ({user}) => {
     // gerenciamento de dados
     const[name, setName] = useState(user ? user.name : '');
     const[email, setEmail] = useState(user ? user.email : '');
-    const[bio, setBio] = useState('');
+    const[bio, setBio] = useState(user ? user.bio : '');
+    const[role, setRole] = useState(user ? user.role : '');
 
     const handleName = (e) =>{
         setName(e.target.value);
@@ -20,7 +21,7 @@ const MyForm = ({user}) => {
     const handleSubmit = (e) =>{
         e.preventDefault();
         console.log("Enviando o formulário");
-        console.log(name, email, bio);
+        console.log(name, email, bio, role);
         
         //limpando formulário
         setName('');
@@ -47,6 +48,16 @@ const MyForm = ({user}) => {
             <label>
                 <span>Bio:</span>
                 <textarea name="" placeholder='Descrição do usuário' onChange={(e) => setBio(e.target.value)} value={bio}></textarea>
+            </label>
+            {/*select*/}
+            <label>
+                <span>Função no sistema</span>
+                <select name="role" onChange={(e) => setRole(e.target.value)} value={role}>
+                    <option value="selecione" disabled>Selecione</option>
+                    <option value="user">Usuário</option>
+                    <option value="editor">Editor</option>
+                    <option value="admin">Admin</option>
+                </select>
             </label>
             <input type="submit" value="Enviar"/>
         </form>
